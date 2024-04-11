ce111a606fa91e9c3ecfafeec8720b4521743a5c
colineckert <colineckert10@gmail.com>
2024-04-11 07:02:10 -0700

    start dashboard cards

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,35 @@
-import AdminLayout from './layout';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 export default function AdminDashbaord() {
-  return <h1>Admin Dashboard</h1>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <DashboardCard title="Sales" subtitle="This Month" body="body" />
+    </div>
+  );
+}
+
+type DashboardCardProps = {
+  title: string;
+  subtitle: string;
+  body: string;
+};
+
+function DashboardCard({ title, subtitle, body }: DashboardCardProps) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+        <CardDescription>{subtitle}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>{body}</p>
+      </CardContent>
+    </Card>
+  );
 }
