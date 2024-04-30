857b1544341fb81bab59f364e06fc4f9af2c26e9
Colin Eckert <colineckert10@gmail.com>
2024-04-30 05:27:03 -0700

    build checkout form UI

--- src/app/(customerFacing)/products/[id]/purchase/page.tsx
+++ src/app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,5 +1,9 @@
 import db from '@/db/db';
 import { notFound } from 'next/navigation';
+import Stripe from 'stripe';
+import { CheckoutForm } from './_components/CheckoutForm';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function PurchasePage({
   params: { id },
@@ -7,8 +11,22 @@ export default async function PurchasePage({
   params: { id: string };
 }) {
   const product = await db.product.findUnique({ where: { id } });
-
   if (product == null) return notFound();
 
-  return <h1>Hi</h1>;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: 'USD',
+    metadata: { productId: product.id },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw new Error('Failed to create payment intent');
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
 }
