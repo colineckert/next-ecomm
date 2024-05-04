c8ad08b33281d8f3cb488619e2565466443b2e56
Colin Eckert <colineckert10@gmail.com>
2024-05-04 14:41:52 -0700

    dispaly payment errors

---
+++ src/app/(customerFacing)/stripe/purchase-success/page.tsx
@@ -0,0 +1,8 @@
+export default function SuccessPage() {
+  return (
+    <div className="max-w-5xl mx-auto space-y-8">
+      <h1 className="text-3xl font-bold">Success!</h1>
+      <p>Your purchase was successful.</p>
+    </div>
+  );
+}
